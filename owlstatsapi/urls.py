b07e4ccf127042ba5b87ef154fd0bda65a645591
Kellen Steffen <kellen.steffen@thalmic.com>
2019-03-27 14:22:18 -0400

    Restore admin router.

--- owlstatsapi/urls.py
+++ owlstatsapi/urls.py
@@ -1,14 +1,16 @@
+from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from owlstatsapi.api import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+v1Router = routers.DefaultRouter(trailing_slash=False)
+v1Router.register(r'users', views.UserViewSet)
+v1Router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('v1/', include(v1Router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('admin/', admin.site.urls),
 ]
