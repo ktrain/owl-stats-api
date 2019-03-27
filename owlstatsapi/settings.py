d014a74ba859ff46b453789ac82171af3d2a9f0a
Kellen Steffen <kellen.steffen@thalmic.com>
2019-03-27 14:18:37 -0400

    Add tutorial views and router.

--- owlstatsapi/settings.py
+++ owlstatsapi/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -122,5 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# REST framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
 
 from .local_settings import *
