1c255d8f6ba15852de3911c620f0ab6b7866c112
Kellen Steffen <kellen.steffen@thalmic.com>
2019-03-28 10:59:23 -0400

    Catch local settings import exception.

--- owlstatsapi/settings.py
+++ owlstatsapi/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import django_heroku
+import warnings
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,9 +135,10 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
 }
 
-from .local_settings import *
+try:
+    from .local_settings import *
+except Exception as e:
+    warnings.warn('Failed to import local settings: {}'.format(e), RuntimeWarning)
 
 django_heroku.settings(locals())
 
-if SECRET_KEY is None:
-    raise Exception('No SECRET_KEY defined.')
