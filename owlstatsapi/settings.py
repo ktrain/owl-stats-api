7b077d3040204e3994d97fdc2e81ffeb7ba9ff4a
Kellen Steffen <kellen.steffen@thalmic.com>
2019-03-28 10:48:33 -0400

    Add django-heroku.

--- owlstatsapi/settings.py
+++ owlstatsapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,6 +135,8 @@ REST_FRAMEWORK = {
 }
 
 from .local_settings import *
+
+django_heroku.settings(locals())
 
 if SECRET_KEY is None:
     raise Exception('No SECRET_KEY defined.')
